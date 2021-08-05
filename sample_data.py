8a98507bed748067a3a1d66c5be4c66f1798591b
oscar-richardson <oscarrichardson1999@gmail.com>
2021-08-05 11:28:50 +0100

    got python working for pilot data

--- sample_data.py
+++ sample_data.py
@@ -1,53 +1,74 @@
 # -*- coding: utf-8 -*-
 
+import datetime as dt
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mpl_dates
-import numpy as np
 import json
+import geopy.distance as gp
+import numpy as np
 import seaborn as sb
 
 
-df = pd.read_csv('D21CE61CAC53_08_Jul_2021_10_33_31.csv', parse_dates=['Date'])
+num_datasets = 4
+parser = lambda x: dt.datetime.strptime(x, '%d/%m/%Y %H:%M')
+df_list = []
 
-
-time_series = df[['Date', 'PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3']]\
-    .dropna(subset=['PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3'])\
+for i in range(num_datasets):
+    new_df = pd.read_csv('Atmotube ' + str(i+1) + ' Data.csv', parse_dates=['Date'], date_parser=parser)
+    new_df = new_df[['Longitude', 'Latitude', 'Date', 'PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3']]\
+    .dropna(subset=['Date', 'PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3'])\
         .reset_index(drop=True)
-        
-plt.figure('Time series')
-plt.style.use('seaborn')
+    df_list.append(new_df)
 
-plt.plot_date(time_series['Date'], time_series['PM1, ug/m3'], linestyle='solid', \
-              marker='', label='PM1')
-plt.plot_date(time_series['Date'], time_series['PM2.5, ug/m3'], linestyle='solid', \
-              marker='', label='PM2.5')
-plt.plot_date(time_series['Date'], time_series['PM10, ug/m3'], linestyle='solid', \
-              marker='', label='PM10')
-
-plt.gcf().autofmt_xdate()
-date_format = mpl_dates.DateFormatter('%d %b %H:%M')
-plt.gca().xaxis.set_major_formatter(date_format)
-plt.title('Time series showing how density of particulates (ug/m3) varies with time')
-plt.xlabel('Date and time')
-plt.ylabel('Density of particulates, ug/m3')
-plt.legend()
-
-time_series['Date'] = time_series['Date'].astype(str)
-time_series.to_json('timeseries.json')
-
-
-heat_map = df[['Longitude', 'Latitude', 'PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3', 'Date']]\
-    .dropna(subset=['Longitude', 'Latitude', 'PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3', 'Date'])\
+heat_map = pd.concat(df_list)
+heat_map = heat_map.dropna(subset=['Longitude', 'Latitude'])\
         .reset_index(drop=True)
 
-num_cuts = 20
-cuts = pd.DataFrame({str(feature) + ' Bin' : pd.cut(heat_map[feature], num_cuts) \
+
+for df in df_list:
+    df.drop(columns=['Longitude', 'Latitude'], inplace=True)
+
+for feature in ['PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3']:
+    
+    plt.figure('Time series ' + feature)
+    plt.style.use('seaborn')
+
+    for i in range(len(df_list)):
+        plt.plot_date(df_list[i]['Date'], df_list[i][feature], 
+                      linestyle='solid', marker='', label='Atmotube ' + str(i+1))
+
+    plt.gcf().autofmt_xdate()
+    date_format = mpl_dates.DateFormatter('%d %b %H:%M')
+    plt.gca().xaxis.set_major_formatter(date_format)
+    plt.title('Time series showing how density of ' + feature + ' varies with time')
+    plt.xlabel('Date and time')
+    plt.ylabel('Density of ' + feature)
+    plt.legend()
+
+for i in range(len(df_list)):
+    df_list[i]['Date'] = df_list[i]['Date'].astype(str)
+    df_list[i].to_json('timeseriesatmotube' + str(i+1) + '.json')
+
+
+heat_map = heat_map[heat_map.Longitude < -1.7979444500000001]
+
+long_min = heat_map['Longitude'].min()
+long_max = heat_map['Longitude'].max()
+long_mid = (long_min + long_max)/2
+lat_min = heat_map['Latitude'].min()
+lat_max = heat_map['Latitude'].max()
+lat_mid = (lat_min + lat_max)/2
+long_distance = gp.distance((lat_mid, long_max), ((lat_mid, long_min))).m
+lat_distance = gp.distance((lat_max, long_mid), ((lat_min, long_mid))).m
+
+grid_size = 1
+num_cuts = {'Longitude': round(long_distance/grid_size), 'Latitude': round(lat_distance/grid_size)}
+cuts = pd.DataFrame({str(feature) + ' Bin' : pd.cut(heat_map[feature], num_cuts[feature]) \
                      for feature in ['Longitude', 'Latitude']}) #creates DF where Long and Lat columns are the bin each datapoint goes in
 groups = heat_map.join(cuts).groupby(list(cuts)) #joins the DFs and divides into groups based on Long and Lat bins
 means = groups.mean()
 means_dropna = means.dropna()
-print(means_dropna)
 start = groups.Date.min().dropna()
 end = groups.Date.max().dropna()
 observations = groups.Date.count().replace(0, np.nan).dropna()
@@ -75,9 +96,17 @@ means = means.iloc[::-1]
 
 for dependent_var in ['PM1, ug/m3', 'PM2.5, ug/m3', 'PM10, ug/m3']:
     plt.figure('Heat map ' + dependent_var)
-    sb.heatmap(means[dependent_var], annot = True, \
+    sb.heatmap(means[dependent_var], \
            xticklabels = means[dependent_var].columns.map(lambda x : x.left), \
            yticklabels = means[dependent_var].index.map(lambda x : x.left), \
                cmap = 'coolwarm')
     plt.title('Heat map showing how density of ' + dependent_var + ' varies with location')
     plt.tight_layout()
+
+"""
+If Long/Lat columns of dataset are empty, we get:
+    
+    ValueError: Cannot cut empty array
+    
+so we need to add some error-checking!
+"""
